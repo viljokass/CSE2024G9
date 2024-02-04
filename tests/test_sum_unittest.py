6a5d54957447cb14d5fdce034e27b36e414ab565
Ville <ville.juhani.hytonen@gmail.com>
2024-02-04 14:00:23 +0200

    added unittests for sum

--- tests/test_sum_unittest.py
+++ tests/test_sum_unittest.py
@@ -8,6 +8,9 @@ class TestSum(unittest.TestCase):
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
+    
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 6)), 9, "Should be 9")
 
 if __name__ == '__main__':
     unittest.main()
