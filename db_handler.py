28827b71636cbab1ccc952e29ff65005069d77b0
olrut <121128961+olrut@users.noreply.github.com>
2024-03-05 18:12:57 +0200

    Mongo db handler (#27)
    * DB-handler and models for order and trade
    
    * Fixed error caused by multiple installs of bson
    
    * Fix ImportError in test_db.py
    
    * Added the creation of env. file to action steps
    
    * Converted db tests to Mock database
    
    * Update python-app.yml
    
    Update python-app workflow

---
+++ db_handler.py
@@ -0,0 +1,89 @@
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+import os
+
+from models.order import Order
+from models.trade import Trade
+
+
+class DbHandler:
+
+    def __init__(self, client=None):
+        """
+        Initialize the database connection
+        :param client: The client to use for the database connection
+        """
+
+        if client is None:
+            # If the environment variable is not set for the test database, set it to the value in the .env file
+            os.environ['db'] = dotenv_values(".env").get('DB_NAME')
+            self.db = MongoClient(os.getenv('mongodb_uri'))[os.getenv('db')]
+        else:
+            self.db = client
+
+    def record_trade(self, trade: Trade):
+        """Record a trade in the database"""
+        try:
+            return self.db.trades.insert_one(trade.json())
+        except Exception as e:
+            return str(e)
+
+    def get_trades(self) -> str:
+        """Get all trades from the database"""
+        try:
+            cursor = list(self.db.trades.find())
+            return dumps(cursor)
+        except Exception as e:
+            return str(e)
+
+    def delete_trade(self, trade_id: str):
+        """Delete a trade from the database by its id"""
+        try:
+            return self.db.trades.delete_one({'_id': trade_id})
+        except Exception as e:
+            return str(e)
+
+    def delete_trades(self):
+        """Delete all trades from the database"""
+        try:
+            return self.db.trades.delete_many({})
+        except Exception as e:
+            return str(e)
+
+    def record_order(self, order: Order):
+        """Record an order in the database"""
+        try:
+            return self.db.orders.insert_one(order.json())
+        except Exception as e:
+            return str(e)
+
+    def delete_order(self, order_id: str):
+        """Delete an order from the database by its id"""
+        try:
+            return self.db.orders.delete_one({'_id': order_id})
+        except Exception as e:
+            return str(e)
+
+    def delete_orders(self):
+        """Delete all orders from the database"""
+        try:
+            return self.db.orders.delete_many({})
+        except Exception as e:
+            return str(e)
+
+    def update_order(self, order_id: str, quantity: int):
+        """Update the quantity of an order"""
+        try:
+            return self.db.orders.update_one({'_id': order_id}, {'$set': {'quantity': quantity, }})
+        except Exception as e:
+            return str(e)
+
+    def get_orders(self):
+        """Get all orders from the database"""
+        try:
+            cursor = list(self.db.orders.find())
+            return dumps(cursor)
+        except Exception as e:
+            return str(e)
