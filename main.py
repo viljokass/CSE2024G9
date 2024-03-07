4c03402b3533f79fa6e85e6a7786a740f59d83f1
Ville <ville.juhani.hytonen@gmail.com>
2024-03-07 15:27:23 +0200

    feat: integrate db to last_traded_price

---
+++ main.py
@@ -0,0 +1,9 @@
+from routes import create_app
+from scheduler import start_scheduler
+from db_handler import DbHandler
+
+    
+db_handler = DbHandler()
+start_scheduler(db_handler)
+app = create_app(db_handler)
+app.run()
