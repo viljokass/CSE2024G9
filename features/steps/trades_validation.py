f301dcd8eda10b1117eab87b0202851b97bcdf82
Ville <ville.juhani.hytonen@gmail.com>
2024-03-10 00:28:19 +0200

    feat: add logic for matching orders

--- features/steps/trades_validation.py
+++ features/steps/trades_validation.py
@@ -6,10 +6,9 @@ def validate_response(context):
 
     trades_json = context.db_handler.get_trades()
     trades = json.loads(trades_json)
-    assert len(trades) == 2
+    assert len(trades) == len(context.table.rows)
     index = 0
     for excpexted_trade in context.table: 
-    
         assert trades[index]['price'] == float(excpexted_trade.cells[1])
         assert trades[index]['quantity'] == int(excpexted_trade.cells[2])
 
